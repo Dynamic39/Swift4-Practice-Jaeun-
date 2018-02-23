1bd0c0de870194446271f2a2e2cf7c62f20f2259
Sam Kyu, Kang <sky4411v@gmail.com>
2018-02-23 11:55:27 +0900

    0223 SideBar DIY Basic

--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/View/FrontViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/View/FrontViewController.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class FrontViewController: UIViewController {
+    
+    //델리게이트 패턴을 사용하여, 클래스 내에 어디서든 참조가 가능하도록 한다.
+    var delegate: RevealViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //버튼 만들기
+        leftSideBarOpenButtonCreate()
+        
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //액션을 실행한 변수를 시행한다.
+    @objc func moveSide(_ sender: Any) {
+        
+        if self.delegate?.isSideBarShowing == false {
+            self.delegate?.openSideBar(nil) // 사이드바 오픈
+        } else {
+            self.delegate?.closeSideBar(nil) // 사이드 바 닫기
+        }
+        
     }
-    */
+    
+    func leftSideBarOpenButtonCreate() {
+        //사이드 바 오픈용 버튼 정의
+        let btnSideBar = UIBarButtonItem(image: UIImage(named: "sidemenu.png"),
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(moveSide(_:)))
+        
+        //버튼 왼쪽 영역에 추가
+        self.navigationItem.leftBarButtonItem = btnSideBar
+    }
 
 }
--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/View/RevealViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/View/RevealViewController.swift
@@ -18,36 +18,114 @@ class RevealViewController: UIViewController {
     
     let SLIDE_TIME = 0.3 // 사이드바 열림 / 닫힘 시간
     let SIDEBAR_WIDTH: CGFloat = 260 // 사이드바 너비
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.setupView()
+        
     }
     
     //초기화면 설정
     func setupView() {
-        
+        //프론트 컨트롤러 객체를 읽어 온다.(네비게이션 컨트롤러 형태로 불러와야, 네비게이션 컨트롤러를 함께 사용할 수 있다.)
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "sw_front") as? UINavigationController {
+            //읽어온 컨트롤러를 클래스 전체에서 참조할 수 있도록 속성 저장
+            self.contentVC = vc
+            //프론트 컨트롤러 객체를 메인 컨트롤러의 자식으로 등록
+            self.addChildViewController(vc)
+            //프론트 컨트롤러 뷰를 메인컨트롤러 서브 뷰로 등록
+            self.view.addSubview(vc.view)
+            //프론트 컨트롤러에 부모뷰 컨트롤러가 바뀌었음을 알려준다.
+            vc.didMove(toParentViewController: self)
+            
+            //프론트 컨트롤의 델리게이트 변수에 참조 정보를 넣어줌
+            let frontVC = vc.viewControllers[0] as? FrontViewController
+            frontVC?.delegate = self
+            
+        }
     }
     
     //사이드바 뷰 로딩
     func getSideView() {
         
+        //사이드바 컨트롤러 객체 읽어오기
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "sw_rear") {
+            //다른 메소드에서 참조 가능하게 속성에 저장
+            self.sideVC = vc
+            //컨테이너 뷰 컨트롤러에 연결
+            self.addChildViewController(vc)
+            self.view.addSubview(vc.view)
+            //부모뷰 컨트롤러가 바뀌었음을 알려줌
+            vc.didMove(toParentViewController: self)
+            //프론트 컨트롤뷰를 가장 위로 올린다.
+            self.view.bringSubview(toFront: (self.contentVC?.view)!)
+        }
     }
     
     //컨텐츠 뷰에 그림자 효과
-    func setShadowEffect() {
+    func setShadowEffect(shadow: Bool, offset: CGFloat) {
+        
+        //그림자 효과
+        if (shadow == true) {
+            self.contentVC?.view.layer.cornerRadius = 10 // 그림자 모서리
+            self.contentVC?.view.layer.shadowOpacity = 0.8 // 그림자 투명도
+            self.contentVC?.view.layer.shadowColor = UIColor.black.cgColor // 그림자 색상
+            self.contentVC?.view.layer.shadowOffset = CGSize(width: offset, height: offset) // 그림자 크기
+        } else {
+            self.contentVC?.view.layer.cornerRadius = 0.0
+            self.contentVC?.view.layer.shadowOffset = CGSize(width: 0, height: 0)
+        }
         
     }
     
     //사이드바 오픈
-    func openSideBar() {
+    func openSideBar(_ complete: (() -> Void)? ) {
         
+        //앞에 정의한 메서드 실행
+        self.getSideView() // 사이드 뷰 실행
+        self.setShadowEffect(shadow: true, offset: -2)
+        
+        //애니매이션 옵션
+        let options = UIViewAnimationOptions([.curveEaseInOut, .beginFromCurrentState])
+        //애니매이션 실행
+        UIView.animate(withDuration: TimeInterval(self.SLIDE_TIME),
+                       delay: TimeInterval(0),
+                       options: options,
+                       animations: {
+                        self.contentVC?.view.frame = CGRect(x: self.SIDEBAR_WIDTH, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        }, completion: {
+            if $0 == true {
+                self.isSideBarShowing = true // 열림상태 플래그 변경
+                complete?() // 함수 완료 처리
+            }
+        })
     }
     
     //사이드바 close
-    func closeSideBar() {
+    func closeSideBar(_ complete: (() -> Void)?) {
+        
+        let options = UIViewAnimationOptions([.curveEaseInOut, .beginFromCurrentState])
+        //애니매이션 실행
+        UIView.animate(withDuration: TimeInterval(self.SLIDE_TIME),
+                       delay: TimeInterval(0),
+                       options: options, animations: {
+                        self.contentVC?.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        },
+                       //완료후 작업 진행
+                       completion: {
+                        if $0 == true {
+                            
+                            self.sideVC?.view.removeFromSuperview() // 사이드바 제거
+                            self.sideVC = nil
+                            self.isSideBarShowing = false // 닫힘상태로 플래그 변경
+                            self.setShadowEffect(shadow: false, offset: 0) // 그림자 효과 삭제
+                            complete?() // 함수 완료처리
+                            
+                        }
+        })
         
     }
     
 }
+
