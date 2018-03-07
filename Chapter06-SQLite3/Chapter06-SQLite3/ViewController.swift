7b4b061496bcef9d5c5af7311f615db965404539
Sam Kyu, Kang <sky4411v@gmail.com>
2018-03-07 14:28:20 +0900

    0307 Code Refactoring for SQL Block

--- Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -12,48 +12,63 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dbPath = self.getDBPath()
+        self.dbExcute(dbPath: dbPath)
+        
+    }
+    
+    //SQL 구문 실행
+    func dbExcute(dbPath: String) {
         //SQLite3 사용
         var db: OpaquePointer? = nil // SQLite 연결정보 저장 객체
         var stmt: OpaquePointer? = nil // 컴파일된 SQL을 담을 객체
+        
+        //SQL Table 작성
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)" // IF NOT EXISTS 를 작성해 줌으로써 중복되서 시퀀스가 생성되지 않도록 조치한다.
+        
+        //함수 5단계를 불러내서 실행함
+        // 1. 함수를 호출하여 DB연결
+        guard sqlite3_open(dbPath, &db) == SQLITE_OK else {
+            print("DataBase Connect Fail!!")
+            return }
+        
+        // SQL 구문 전달 준비, 컴파일된 SQL 구문객체 생성(=stmt)
+        guard sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK else {
+            print("Prepate Statment Fail!!")
+            return }
+        
+        //DB 연결 종료는 Defer를 사용하여 진행한다.
+        defer {
+            //DB연결 종료, db 객체가 해제됨
+            sqlite3_close(db)
+        }
+        
+        // 컴파일된 SQL 구문 객체를 DB에 전달함
+        if sqlite3_step(stmt) == SQLITE_DONE { print("Create Table Success") }
+        
+        //stmt 연결 종료는 Defer를 사용하여 진행한다.
+        defer {
+            // SQL 구문 삭제 이과정에서 stmt해제
+            sqlite3_finalize(stmt)
+            print("SQL DATA ALL CLOSE!")
+        }
+    }
+    
+    //PATH 를 구하기 위해 새로운 메서드 작성
+    func getDBPath() -> String {
         
         //앱 내 디렉터리 경로에서 SQLite DB파일을 찾는다
         let fileMgr = FileManager() // 파일 매니저 경로 생성
         let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first! // 앱내 문서 디렉터리 경로 검색 및 URL객체 생성
         let dbPath = try! docPathURL.appendingPathComponent("db.sqlite").path // URL객체내에 db.sqlite 파일 경로를 추가한 SQLite 경로 생성
         
-        
         //dbPath에 경로가 없을 경우 앱 번들에서 가져와 복사한다.
         if fileMgr.fileExists(atPath: dbPath) == false {
             let dbSource = Bundle.main.path(forResource: "db", ofType: "sqlite")
             try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
-            
-            print(dbPath)
         }
-        
-        //SQL Table 작성
-        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)" // IF NOT EXISTS 를 작성해 줌으로써 중복되서 시퀀스가 생성되지 않도록 조치한다.
-        
-        //함수 5단계를 불러내서 실행함
-        if sqlite3_open(dbPath, &db) == SQLITE_OK { // 1. 함수를 호출하여 DB연결
-            if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK { // SQL 구문 전달 준비, 컴파일된 SQL 구문객체 생성(=stmt)
-                if sqlite3_step(stmt) == SQLITE_DONE { // 컴파일된 SQL 구문 객체를 DB에 전달함
-                    print("Create Table Success")
-                }
-                sqlite3_finalize(stmt) // SQL 구문 삭제 이과정에서 stmt해제
-            } else {
-                print("Prepate Statment Fail!!")
-            }
-            sqlite3_close(db) // DB연결 종료, db 객체가 해제됨
-            print("SQL DATA ALL CLOSE!")
-        } else {
-            print("DataBase Connect Fail!!")
-            return
-        }
-        
-
-        
-        
-        
+        return dbPath
     }
 
 }
